cbbfde1027131fbf32459dfc45b792adc0ec4c76
Bruno Pinto <brosapinto@gmail.com>
2019-11-19 00:06:18 +0000

    feat: use state hook

--- src/didact.js
+++ src/didact.js
@@ -202,7 +202,45 @@ function reconcileChildren(wipFiber, elements) {
   }
 }
 
+let wipFiber = null;
+let hookIndex = null;
+
+function useState(initial) {
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: []
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(action => {
+    hook.state = action(hook.state);
+  });
+
+  const setState = action => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  };
+
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
+}
+
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
+
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
 }
@@ -261,7 +299,8 @@ requestIdleCallback(workLoop);
 
 const Didact = {
   createElement,
-  render
+  render,
+  useState
 };
 
 export default Didact;
